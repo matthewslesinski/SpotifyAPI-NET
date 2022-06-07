cd0e1f7056879e230700d69900641bf0ad8b8ed0
matthewslesinski <mslesinski12@gmail.com>
2022-06-07 10:03:29 -0400

    Overriding equals and hashcode for AuthorizationCodeTokenResponse

--- SpotifyAPI.Web/Models/Response/AuthorizationCodeTokenResponse.cs
+++ SpotifyAPI.Web/Models/Response/AuthorizationCodeTokenResponse.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace SpotifyAPI.Web
 {
-  public class AuthorizationCodeTokenResponse: IRefreshableToken
+  public class AuthorizationCodeTokenResponse: IRefreshableToken, IEquatable<AuthorizationCodeTokenResponse>
   {
     public string AccessToken { get; set; } = default!;
     public string TokenType { get; set; } = default!;
@@ -17,6 +17,20 @@ namespace SpotifyAPI.Web
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public bool IsExpired { get => CreatedAt.AddSeconds(ExpiresIn) <= DateTime.UtcNow; }
+
+    public override bool Equals(object? obj) => this == obj || (obj != null && obj is AuthorizationCodeTokenResponse other && Equals(other));
+
+    public bool Equals(AuthorizationCodeTokenResponse? other) {
+        return this == other || (other != null
+        && Equals(AccessToken, other.AccessToken)
+				&& Equals(RefreshToken, other.RefreshToken)
+				&& Equals(CreatedAt, other.CreatedAt)
+				&& Equals(ExpiresIn, other.ExpiresIn)
+				&& Equals(TokenType, other.TokenType)
+				&& Equals(Scope, other.Scope));
+    }
+
+    public override int GetHashCode() => HashCode.Combine(AccessToken, TokenType, ExpiresIn, Scope, RefreshToken, CreatedAt, IsExpired);
   }
 }
 
